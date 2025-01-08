68d5ec5e670124e4356dcd65f79247bea2e75698
antenor-z <antenor-z@protonmail.com>
2025-01-08 18:39:30 +0000

    fade in and color change animation

--- contents/randomColor.js
+++ contents/randomColor.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const r = document.querySelector(':root')
-    const colors = ["#329442", "#943232", "#948532", "#3940af", "#943292"]
-    const rand = Math.floor((Math.random() * 100)) % colors.length
-    r.style.setProperty("--primary-color", colors[rand])
-    r.style.setProperty("--primary-color-transparent", colors[rand] + "30") 
-})
+    const r = document.querySelector(':root');
+    const colors = ["#329442", "#943232", "#948532", "#3940af", "#943292"];
+    
+    function updateColors() {
+        const rand = Math.floor(Math.random() * colors.length);
+        r.style.setProperty("--primary-color", colors[rand]);
+        r.style.setProperty("--primary-color-transparent", colors[rand] + "30");
+    }
+
+    updateColors();
+
+    setInterval(updateColors, 60000);
+});
+
