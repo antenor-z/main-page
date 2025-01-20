6457b744ece78cb1ea0cadf393ade4a2b698ba67
antenor-z <antenor-z@pm.me>
2025-01-20 20:45:31 -0300

    reducing delay time

--- contents/loader.js
+++ contents/loader.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", (event) => {
     const loader = document.getElementById("loader");
     loader.style.transition = "opacity 0.7s ease";
-    setTimeout(() => loader.style.opacity = "0", 1000);
-    setTimeout(() => loader.remove(), 3000);
+    setTimeout(() => loader.style.opacity = "0", 100);
+    setTimeout(() => loader.remove(), 2000);
 });
